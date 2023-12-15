f393989a3c86cbdacdde96adb6ef088a404a14d4
Enkot <taras.batenkov@gmail.com>
2023-12-15 21:55:08 +0200

    fix: remove pinia

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -1,10 +1,7 @@
 export default defineNuxtConfig({
-  modules: [
-    '../src/module',
-    '@pinia/nuxt',
-  ],
+  modules: ['../src/module'],
   extends: ['./pets'],
-  devtools: { enabled: false },
+  devtools: { enabled: true },
   openFetch: {
     clients: {
       foo: {
